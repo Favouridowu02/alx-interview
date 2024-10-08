e92a839236988bc5f6ede4a69b4690e114f943de
Favour Idowu <favouridowu02@gmail.com>
2024-10-08 21:21:24 +0100

    Add a function in the file  that returns a pascal value based on the passed in value

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,10 +16,11 @@ def pascal_triangle(n):
     """
     new_list = []
     if n > 0:
-        for i in range(0, n):
-            new_list.append([])
-            new_list[i].append(1)
-            for j in range(0, i//2):
-                new_list[i].append(new_list[i-1][j] + new_list[i-1][(-1 *j)])
-            new_list[i].append(1)
+        for i in range(n):
+            new_list.append([1])
+            for j in range(1, i):
+                new_list[i].append(new_list[i-1][j-1] + new_list[i-1][j])
+            if i > 0:
+                new_list.append(1)
+
     return (new_list)
