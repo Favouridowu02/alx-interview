8cbdda7f09437cc86125a4a799b676c2a914f556
Favour Idowu <favouridowu02@gmail.com>
2024-10-08 20:43:09 +0100

    Add a function in the file  that returns a pascal value based on the passed in value

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    This Module contains a function that Returns a list of integers representing the Pascal's triangle of n
+"""
+
+
+def pascal_triangle(n):
+    """
+        This Function is used to generate a pascal triangle using a List
+        
+        Argument:
+            - n: this is the integer used in generating the length of the pascal triangle.
+        
+        Return: This Function returns the Generated list of the Pascal Value based on the value of the n
+                IF n <= 0. an empty list is returned.
+    """
+    new_list = []
+    if n > 0:
+        for i in range(0, n):
+            new_list.append([])
+            new_list[i].append(1)
+            for j in range(0, i):
+                new_list[i].append(new_list[i-1][j] + new_list[i-1][j-1])
+            new_list[i].append(1)
+    return (new_list)
