330ee1f0c0dba528bdf4123caa3b43757cbaba4f
Favour Idowu <favouridowu02@gmail.com>
2024-11-21 12:08:25 +0100

    Add the readme file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    This Module Contains a function canUnlockAll
+"""
+
+"""
+    Task of todayv is toi write a code that checks hich lockboxes can be opened
+
+    boxes: List of Lists
+
+    All keys woll be a positive number
+    iit first boxes will be unlocked
+"""
+
+def canUnloackAll(boxes):
+    """
+        This function is used to solve the Lockboxes
+
+        Arguments:
+            boxes: The boxes
+    """
+    if 
