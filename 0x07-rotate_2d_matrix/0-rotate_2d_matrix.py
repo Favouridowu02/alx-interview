8c68bf74cb70ac57360650f3ce3339e2301e94d3
Favour Idowu <favouridowu02@gmail.com>
2025-01-09 14:01:16 +0100

    Add a file 0-rotate_2d_matrix
    Implemented a method rotate_2d_matrix(matrix): This method is used to rotate a matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,5 +12,24 @@ def rotate_2d_matrix(matrix):
         Arguments:
             matrix: the 2-Dimensional Matrix to be rotated
     """
-    matrix.reverse()
-    matrix = [[row[i] for row in matrix] for i in range(len(matrix))]
+    if type(matrix) != list:
+        return
+    if len(matrix) <= 0:
+        return
+    if not all(map(lambda x: type(x) == list, matrix)):
+        return
+    rows = len(matrix)
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
+        return
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
+        if i % rows == 0:
+            matrix.append([])
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
--- 0x07-rotate_2d_matrix/test.py
+++ 0x07-rotate_2d_matrix/test.py
@@ -6,13 +6,14 @@
 
 def rotate(matrix):
     matrix.reverse()
-    # return [[row[i] for row in matrix] for i in range(len(matrix) - 1, -1, -1)]
+    return [[row[i] for row in matrix] for i in range(len(matrix) - 1, -1, -1)]
+    # matrix.reverse()
     # return [[row[i] for row in matrix.reverse()] for i in range(len(matrix) - 1, -1, -1)]
-    matrix = [[row[i] for row in matrix] for i in range(len(matrix))]
+    # matrix = [[row[i] for row in matrix] for i in range(len(matrix))]
 
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(matrix,"\n\n")
-# matrix = rotate(matrix)
-rotate(matrix)
+matrix = rotate(matrix)
+# rotate(matrix)
 print(matrix)
