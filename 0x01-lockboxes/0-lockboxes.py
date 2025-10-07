157d631050ef12004edcf7ef2f5749126b23ef57
Favour Idowu <favouridowu02@gmail.com>
2025-10-07 03:26:13 +0100

    .

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,20 +3,24 @@
     This Module Contains a function canUnlockAll
 """
 
-"""
-    Task of todayv is toi write a code that checks hich lockboxes can be opened
 
-    boxes: List of Lists
-
-    All keys woll be a positive number
-    iit first boxes will be unlocked
-"""
-
-def canUnloackAll(boxes):
+def canUnlockAll(boxes):
     """
         This function is used to solve the Lockboxes
 
         Arguments:
             boxes: The boxes
     """
-    if 
+    key = set(boxes[0])
+    #print(key)
+    if len(key) == 0: return False
+    i = 0
+    while i < len(key):
+        key.update(boxes[key[i]])
+        #print(key)
+        i += 1
+    print(key) 
+    if len(key) < len(boxes) - 1 or max(key) != len(boxes) - 1:
+        return False
+    if len(key) == len(boxes) - 1 and max(key) == len(boxes) - 1:
+        return True
--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,7 +12,27 @@ def validUTF8(data):
         Arguments:
             data: This is the data
     """
-    for i in data:
-        if i > (128):
-            return False
-    return True
+    def number_of_bytes(byte):
+        """
+        """
+        if (byte >> 7) == 0b0:
+            return 1
+        elif (byte >> 5) == 0b110:
+            return 2
+        elif (byte >> 4) == 0b1110:
+            return 3
+        elif (byte >> 3) == 0b11110:
+            return 4
+        return 0
+
+    expected_byte = 0
+    for byte in data:
+        if expected_byte == 0:
+            expected_byte = number_of_bytes(byte)
+            if expected_byte == 0:
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            expected_byte -= 1 
+    return expected_byte == 0
